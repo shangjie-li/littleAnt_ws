efb8d3bf4dbebe7d6f6d9f75bb0fe70698947f3f
shangjie-li <1814481684@qq.com>
2021-05-14 13:07:38 +0800

    Add shell file

--- src/driverless/launch/driverless.sh
+++ src/driverless/launch/driverless.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-gnome-terminal \
---tab -e 'sh ~/projects/littleAnt_ws/src/drivers/rtcm3.2/main' \
---tab -e 'roslaunch ~/projects/littleAnt_ws/src/path_planning/launch/path_planning.launch' \
---tab -e 'roslaunch ~/projects/littleAnt_ws/src/driverless/launch/driverless.launch' \
---tab -e 'rviz'
+gnome-terminal -x bash -c "cd /home/seu/projects/littleAnt_ws/src/drivers/rtcm3.2 && ./main; exec bash"
+sleep 1
+
+gnome-terminal -x bash -c "roslaunch /home/seu/projects/littleAnt_ws/src/path_planning/launch/path_planning.launch; exec bash"
+sleep 1
+
+gnome-terminal -x bash -c "roslaunch /home/seu/projects/littleAnt_ws/src/driverless/launch/driverless.launch; exec bash"
+sleep 1
+
+gnome-terminal -x bash -c "rviz; exec bash"
+
