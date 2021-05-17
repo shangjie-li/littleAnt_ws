f4d704b65ab54aac9a164daca5c8f53005384c64
shangjie-li <1814481684@qq.com>
2021-05-17 17:39:16 +0800

    Modified launch file

--- src/driverless/launch/driverless.sh
+++ src/driverless/launch/driverless.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-gnome-terminal -x bash -c "cd /home/seu/projects/littleAnt_ws/src/drivers/rtcm3.2 && ./main; exec bash"
-sleep 1
-
-gnome-terminal -x bash -c "roslaunch /home/seu/projects/littleAnt_ws/src/path_planning/launch/path_planning.launch; exec bash"
-sleep 1
-
-gnome-terminal -x bash -c "roslaunch /home/seu/projects/littleAnt_ws/src/driverless/launch/driverless.launch; exec bash"
-sleep 1
-
-gnome-terminal -x bash -c "rviz; exec bash"
+gnome-terminal --tab -e "sh /home/seu/projects/littleAnt_ws/src/driverless/launch/rtcm.sh" --tab -e "roslaunch /home/seu/projects/littleAnt_ws/src/path_planning/launch/path_planning.launch" --tab -e "roslaunch /home/seu/projects/littleAnt_ws/src/driverless/launch/driverless.launch" --tab -e "rviz"
 
